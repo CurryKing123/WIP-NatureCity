2ca6fb5cee2677eeba0eb2d06a3f32c78f457e1d
CurryKing123 <isaac.park@hotmail.com>
2023-12-27 16:37:04 -0500

    Duplicate registration
    Can't post duplicate usernames now

--- WIP/Assets/Scripts/Main Menu/Registration.cs
+++ WIP/Assets/Scripts/Main Menu/Registration.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class Registration : MonoBehaviour
 {
     public InputField usernameField;
     public InputField passwordField;
     public Button registerButton;
+    public Button backButton;
     public void CallRegister()
     {
         StartCoroutine(Register());
     }
     
+    ///Register Button Interaction
     IEnumerator Register()
     {
         using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8002/account/post-account", "{ \"username\": \"" + usernameField.text + "\", \"password\": \"" + usernameField.text + "\" }", "application/json"))
@@ -37,5 +40,10 @@ public class Registration : MonoBehaviour
     public void VerifyInputs()
     {
         registerButton.interactable = (usernameField.text.Length >= 8 && passwordField.text.Length >= 8);
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene(0);
     }
 }
