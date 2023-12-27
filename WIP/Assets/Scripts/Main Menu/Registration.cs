f258b8eb02123f7135a905862eccc9b90687f605
CurryKing123 <isaac.park@hotmail.com>
2023-12-26 19:33:56 -0500

    fields aren't locating
    Posting to api works, values are all NULL

--- WIP/Assets/Scripts/Main Menu/Registration.cs
+++ WIP/Assets/Scripts/Main Menu/Registration.cs
@@ -6,22 +6,23 @@ using UnityEngine.Networking;
 
 public class Registration : MonoBehaviour
 {
-
     public InputField usernameField;
     public InputField passwordField;
     public Button registerButton;
-
     public void CallRegister()
     {
         StartCoroutine(Register());
     }
-
+    
     IEnumerator Register()
     {
         WWWForm form = new WWWForm();
         form.AddField("username", usernameField.text);
         form.AddField("password", passwordField.text);
+
         UnityWebRequest www = UnityWebRequest.Post("http://localhost:8002/account/post-account", form);
+        
+        www.SetRequestHeader("key","1");
         yield return www.SendWebRequest();
 
         if (www.result != UnityWebRequest.Result.Success)
@@ -33,6 +34,8 @@ public class Registration : MonoBehaviour
             Debug.Log("Form upload complete!");
         }
     }
+
+    
 
     public void VerifyInputs()
     {
